d9c397536898815a6eee3e64da862d0a7d5b40c0
janaka <jnk.madusanka86@gmail.com>
2024-01-09 14:44:58 +0530

    loging form UI created

---
+++ Projects/e-and-e-pos/src/main/java/AppInitializer.java
@@ -0,0 +1,9 @@
+package edu.icet;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class AppInitializer {
+    public static void main(String[] args) {
+        System.out.println("Jna");
+    }
+}
---
+++ Projects/e-and-e-pos/src/main/java/controller/LoginFormController.java
@@ -0,0 +1,21 @@
+package controller;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class LoginFormController {
+
+    @FXML
+    private BorderPane paneLogin;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private JFXButton btnLogin;
+
+}
