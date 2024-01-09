6193466d020775027f3a9c73e38f912138cd541e
janaka <jnk.madusanka86@gmail.com>
2024-01-09 20:36:15 +0530

    profile Forme UI Created

---
+++ Projects/e-and-e-pos/src/main/java/controller/ProfileFormController.java
@@ -0,0 +1,105 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+public class AdminProfileController {
+
+    @FXML
+    private BorderPane paneMyAccount;
+
+    @FXML
+    private JFXButton btnExit;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private JFXButton btnLogin11;
+
+    @FXML
+    private JFXButton btnLogin12;
+
+    @FXML
+    private JFXButton btnLogin13;
+
+    @FXML
+    private JFXButton btnLogin14;
+
+    @FXML
+    private JFXButton btnLogin15;
+
+    @FXML
+    private JFXButton btnLogin16;
+
+    @FXML
+    private JFXButton btnLogin17;
+
+    @FXML
+    private JFXButton btnCustomers;
+
+    @FXML
+    private JFXButton btnRepairDetail;
+
+    @FXML
+    private JFXButton btnPlaceRepairOrder;
+
+    @FXML
+    private JFXButton btnParts;
+
+    @FXML
+    private JFXButton BtnReports;
+
+    @FXML
+    private JFXButton btnUserRegistration;
+
+    @FXML
+    private JFXButton btnMyAccount;
+
+    @FXML
+    private ImageView imgProfile;
+
+    @FXML
+    private Label lblProfileName;
+
+    @FXML
+    private Label lblProfileRole;
+
+    @FXML
+    private Label lblUserProfileHedding;
+
+    @FXML
+    private Label lblUserId;
+
+    @FXML
+    private Label lblMobile;
+
+    @FXML
+    private Label LblNIC;
+
+    @FXML
+    private Label lblEmail;
+
+    @FXML
+    private Label lblRole;
+
+    @FXML
+    private Label lblRoleId;
+
+    @FXML
+    private Label lblUserName;
+
+    @FXML
+    private TextField lblNewPassword1;
+
+    @FXML
+    private TextField txtOldPassword;
+
+    @FXML
+    private JFXButton btnSave;
+
+}
